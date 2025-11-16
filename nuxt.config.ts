ead6cdfe39e83b3a3c045a3d5e767a91309c839f
Blede <bledegraphics@gmail.com>
2025-11-15 22:45:55 -0500

    feat: Basic i18n set up

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,117 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+	compatibilityDate: "2025-07-15",
+	devtools: { enabled: true },
 
-  nitro: {
-    preset: "cloudflare_module",
+	nitro: {
+		preset: "cloudflare_module",
 
-    cloudflare: {
-      deployConfig: true,
-      nodeCompat: true
-    }
-  },
+		cloudflare: {
+			deployConfig: true,
+			nodeCompat: true,
+		},
+	},
 
-  modules: ["nitro-cloudflare-dev"]
-})
+	modules: [
+		"nitro-cloudflare-dev",
+		"@nuxtjs/i18n",
+		"nuxt-auth-sanctum",
+		"nuxt-sanctum-precognition",
+		"@nuxtjs/seo",
+	],
+	i18n: {
+		strategy: "prefix_except_default",
+		detectBrowserLanguage: {
+			useCookie: true,
+			cookieKey: "talos_i18n_redirected",
+			redirectOn: "root",
+		},
+		defaultLocale: "en",
+		locales: [
+			{
+				code: "en",
+				name: "English",
+				file: "en.json",
+				language: "en",
+			},
+			{
+				code: "ja",
+				name: "Japanese",
+				file: "ja.json",
+				language: "ja",
+			},
+			{
+				code: "ko",
+				name: "Korean",
+				file: "ko.json",
+				language: "ko",
+			},
+			{
+				code: "zh-TW",
+				name: "Chinese Traditional",
+				file: "zh-TW.json",
+				language: "zh-TW",
+			},
+			{
+				code: "zh-CN",
+				name: "Chinese Simplified",
+				file: "zh-CN.json",
+				language: "zh-CN",
+			},
+			{
+				code: "es",
+				name: "Spanish",
+				file: "es.json",
+				language: "es",
+			},
+			{
+				code: "pt",
+				name: "Portuguese",
+				file: "pt.json",
+				language: "pt",
+			},
+			{
+				code: "fr",
+				name: "French",
+				file: "fr.json",
+				language: "fr",
+			},
+			{
+				code: "de",
+				name: "German",
+				file: "de.json",
+				language: "de",
+			},
+			{
+				code: "ru",
+				name: "Russian",
+				file: "ru.json",
+				language: "ru",
+			},
+			{
+				code: "it",
+				name: "Italian",
+				file: "it.json",
+				language: "it",
+			},
+			{
+				code: "id",
+				name: "Indonesian",
+				file: "id.json",
+				language: "id",
+			},
+			{
+				code: "th",
+				name: "Thai",
+				file: "th.json",
+				language: "th",
+			},
+			{
+				code: "vi",
+				name: "Vietnamese",
+				file: "vi.json",
+				language: "vi",
+			},
+		],
+	},
+});
