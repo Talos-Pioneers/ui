dfae01151a8a71afc10eb229e2076d4696a66471
Blede <bledegraphics@gmail.com>
2025-11-18 22:37:50 -0500

    feat: Added extra icons

---
+++ app/components/icons/RegionValleyIcon.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+<template>
+    <svg width="54" height="24" viewBox="0 0 54 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+            d="M53.9187 23.4923C48.2186 14.7593 38.5112 9.01163 27.4913 9.01163C16.4715 9.01163 6.77188 14.7593 1.0717 23.4923H53.9187Z"
+            fill="black" />
+        <path
+            d="M29.9609 8.20759C26.7303 3.25597 21.2242 0 14.9804 0C8.73665 0 3.23062 3.25597 0 8.20759H29.9531H29.9609Z"
+            fill="black" />
+    </svg>
+</template>
