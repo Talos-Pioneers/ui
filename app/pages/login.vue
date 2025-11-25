c5275f2fd5b0f795626ca0d7fb555114e0f58ae3
Blede <bledegraphics@gmail.com>
2025-11-24 20:39:13 -0500

    Auth forms as single pages

---
+++ app/pages/login.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import LoginForm from '~/components/auth/LoginForm.vue';
+</script>
+
+<template>
+    <div class="flex justify-center items-center h-[calc(100vh-var(--header-height))]">
+        <LoginForm />
+    </div>
+</template>
--- app/pages/register.vue
+++ app/pages/register.vue
@@ -4,5 +4,7 @@ import RegisterForm from '~/components/auth/RegisterForm.vue';
 </script>
 
 <template>
-    <RegisterForm />
+    <div class="flex pt-10 justify-center items-center h-[calc(100vh-var(--header-height))]">
+        <RegisterForm />
+    </div>
 </template>
