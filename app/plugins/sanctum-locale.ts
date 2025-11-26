8e9ab1ff4d15d43c3aa82dbef5bb6a3f8a88abe5
Yurihaia <17830663+Yurihaia@users.noreply.github.com>
2025-11-26 00:07:34 -0500

    fix: Some translatable text not changing until page refresh

--- app/plugins/sanctum-locale.ts
+++ app/plugins/sanctum-locale.ts
@@ -1,9 +1,9 @@
 export default defineNuxtPlugin((nuxtApp) => {
-	const locale = nuxtApp.$i18n.locale.value;
+	const locale = nuxtApp.$i18n.locale;
 
 	nuxtApp.hook("sanctum:request", (app, ctx) => {
 		// Get the current locale from i18n
 		// Add the current locale to the request headers
-		ctx.options.headers.set("X-Locale", locale);
+		ctx.options.headers.set("X-Locale", locale.value);
 	});
 });
