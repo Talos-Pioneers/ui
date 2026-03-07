76f9de17a30da2a39c175aaedb756a0e069a82e6
Blede <bledegraphics@gmail.com>
2026-03-07 10:16:09 -0500

    Copy Updates

--- app/pages/(content)/about.vue
+++ app/pages/(content)/about.vue
@@ -30,7 +30,7 @@ const teamMembers = [
 		name: '十二獣Zodiac',
 		role: 'illustrator',
 		avatar: '/avatars/zodiac.jpg',
-	}
+	},
 ]
 
 // Special thanks
@@ -55,6 +55,29 @@ useHead({
 		},
 	],
 })
+
+const friends = [
+	{
+		name: 'Sanity Gone',
+		url: 'https://sanitygone.help/',
+	},
+	{
+		name: 'Warfarin Wiki',
+		url: 'https://warfarin.wiki/',
+	},
+	{
+		name: 'Open Endfield Map',
+		url: 'https://opendfieldmap.org/',
+	},
+	{
+		name: 'Enka Network',
+		url: 'https://enka.network/?ef',
+	},
+	{
+		name: 'Data Field',
+		url: 'https://datafield.cc/',
+	},
+]
 </script>
 
 <template>
@@ -117,6 +140,28 @@ useHead({
 								</div>
 							</div>
 						</div>
+					</div>
+					<div class="mt-6">
+						<a
+							href="https://discord.gg/apeehhry"
+							target="_blank"
+							rel="noopener noreferrer"
+							class="flex items-center gap-2 text-xl border bg-white justify-center p-4 hover:bg-accent transition-all"
+						>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="24"
+								height="24"
+								fill="currentColor"
+								class="bi bi-discord"
+								viewBox="0 0 16 16"
+							>
+								<path
+									d="M13.545 2.907a13.2 13.2 0 0 0-3.257-1.011.05.05 0 0 0-.052.025c-.141.25-.297.577-.406.833a12.2 12.2 0 0 0-3.658 0 8 8 0 0 0-.412-.833.05.05 0 0 0-.052-.025c-1.125.194-2.22.534-3.257 1.011a.04.04 0 0 0-.021.018C.356 6.024-.213 9.047.066 12.032q.003.022.021.037a13.3 13.3 0 0 0 3.995 2.02.05.05 0 0 0 .056-.019q.463-.63.818-1.329a.05.05 0 0 0-.01-.059l-.018-.011a9 9 0 0 1-1.248-.595.05.05 0 0 1-.02-.066l.015-.019q.127-.095.248-.195a.05.05 0 0 1 .051-.007c2.619 1.196 5.454 1.196 8.041 0a.05.05 0 0 1 .053.007q.121.1.248.195a.05.05 0 0 1-.004.085 8 8 0 0 1-1.249.594.05.05 0 0 0-.03.03.05.05 0 0 0 .003.041c.24.465.515.909.817 1.329a.05.05 0 0 0 .056.019 13.2 13.2 0 0 0 4.001-2.02.05.05 0 0 0 .021-.037c.334-3.451-.559-6.449-2.366-9.106a.03.03 0 0 0-.02-.019m-8.198 7.307c-.789 0-1.438-.724-1.438-1.612s.637-1.613 1.438-1.613c.807 0 1.45.73 1.438 1.613 0 .888-.637 1.612-1.438 1.612m5.316 0c-.788 0-1.438-.724-1.438-1.612s.637-1.613 1.438-1.613c.807 0 1.451.73 1.438 1.613 0 .888-.631 1.612-1.438 1.612"
+								/>
+							</svg>
+							Join us on Discord
+						</a>
 					</div>
 				</aside>
 
@@ -213,6 +258,29 @@ useHead({
 						>
 							<li v-for="person in specialThanks" :key="person">
 								{{ person }}
+							</li>
+						</ul>
+					</section>
+
+					<section
+						class="bg-white dark:bg-cool-gray-90 rounded-lg border border-cool-gray-20 dark:border-cool-gray-80 p-6 space-y-4"
+					>
+						<h2
+							class="font-bold text-xl text-cool-gray-80 dark:text-cool-gray-20"
+						>
+							Friends
+						</h2>
+						<ul
+							class="list-disc list-inside space-y-2 text-cool-gray-70 dark:text-cool-gray-30"
+						>
+							<li v-for="friend in friends" :key="friend.name">
+								<a
+									:href="friend.url"
+									target="_blank"
+									class="text-cool-gray-80 hover:text-cool-gray-80 hover:underline transition-colors"
+								>
+									{{ friend.name }}
+								</a>
 							</li>
 						</ul>
 					</section>
